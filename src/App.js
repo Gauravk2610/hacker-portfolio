06310cf4b52541f5b37a8da91ecc2c8b899bbc71
Gaurav konde <63660013+Gauravk2610@users.noreply.github.com>
2022-06-22 01:35:58 +0530

    rgb scroll

--- src/App.js
+++ src/App.js
@@ -27,16 +27,28 @@ function App() {
 
   // }, [])
 
-  return (
-    <div className="App">
-      <Router>
-        <Header />
-        <AnimatePresence exitBeforeEnter>
-          <AnimatedRoutes />
-        </AnimatePresence>
-      </Router>
+  useEffect(() => {
+    let progress = document.getElementById('progressbar')
+    let totalHeight = document.body.scrollHeight - window.innerHeight;
+    window.onscroll = function() {
+      let progressHeight = (window.pageYOffset / totalHeight) * 100;
+      progress.style.height = progressHeight + "%"
+    }
+  }, [])
 
-    </div>
+  return (
+    <>
+      <div id='progressbar' />
+      <div id='scrollPath' />
+      <div className="App">
+        <Router>
+          <Header />
+          <AnimatePresence exitBeforeEnter>
+            <AnimatedRoutes />
+          </AnimatePresence>
+        </Router>
+      </div>
+    </>
   );
 }
 
